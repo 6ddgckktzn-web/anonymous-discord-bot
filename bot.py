5cf12e488a4b183570d5335439d5f733e6c29c02
6ddgckktzn-web <6ddgckktzn@privaterelay.appleid.com>
2026-06-25 16:50:59 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import discord
+from discord.ext import commands
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} 로그인 완료!")
+
+bot.run(TOKEN)
