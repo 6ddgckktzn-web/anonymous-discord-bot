8a7b9c4242606c8b65bec900ad8d04cd2901888c
6ddgckktzn-web <6ddgckktzn@privaterelay.appleid.com>
2026-06-25 17:00:54 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,36 @@ from discord.ext import commands
 
 TOKEN = os.getenv("TOKEN")
 
+ANON_CHANNEL_ID = 1519593563178926150
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} 로그인 완료!")
+
+@bot.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+
+    if message.channel.id != ANON_CHANNEL_ID:
+        return
+
+    try:
+        await message.delete()
+    except:
+        pass
+
+    await message.channel.send(
+        f"🌸 익명\n\n{message.content}"
+    )
 
 bot.run(TOKEN)
